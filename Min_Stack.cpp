d8fb2866e2448e63e76ac9c320816c948fd99921
Snehlata <145580516+Snehlata1111@users.noreply.github.com>
2025-07-30 22:14:28 +0530

    Update Min_Stack.cpp

--- Min_Stack.cpp
+++ Min_Stack.cpp
@@ -32,3 +32,57 @@ public:
         return 1;
     }
 };
+
+/------------------------------------------------------------------------------------------------------------------------------------------------------------/
+    
+//with space complexity O(n)
+
+class MinStack {
+public:
+    stack<long long int> s ;
+    long long int minVal ;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s.empty()){
+        s.push(val) ;
+        minVal = val ;
+        }
+        else
+        {
+            if(val<minVal) {
+            s.push((long long)2*val - minVal) ;
+            minVal=val ;
+            }
+            else{
+                s.push(val) ;
+            }
+        }
+    }
+    
+    void pop() {
+        if(s.top()<minVal){
+        minVal = 2*minVal - s.top();
+        }
+
+        s.pop() ;
+    }
+    
+    int top() {
+        if(s.top()<minVal){
+            return minVal ;
+        }
+
+        else{
+            return s.top() ;
+        }
+        
+    }
+    
+    int getMin() {
+        return minVal ;
+    }
+};
